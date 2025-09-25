3e043a7bd041dbd06da21c27e7169b8f44b9c4b2
Andre Holtz <andregholtz81@gmail.com>
2025-09-25 11:41:16 -0300

    comentado

--- botoes.py
+++ botoes.py
@@ -1,19 +1,18 @@
 import time
 import gpiod
 from gpiod.line import Direction, Value
-from portas import Porta  # Importa sua biblioteca de portas
+from portas import Porta  
 
 class Botao:
     """
-    Classe para gerenciar botões conectados na Banana Pi M4 Zero via GPIO.
-    Cada botão retorna apenas dois estados: pressionado (0) ou liberado (1).
+    Classe para gerenciar botões conectados na Banana Pi M4 Zero via GPIO
+    Cada botão retorna apenas dois estados: pressionado (0) ou liberado (1)
     """
 
     def __init__(self, portas):
         """
-        Inicializa o driver dos botões para as portas desejadas.
-
-        :param portas: tupla ou lista com as portas a serem utilizadas (ex: (Porta.P2, Porta.P3))
+        Inicializa o driver dos botões para as portas desejadas
+        portas: tupla ou lista com as portas a serem utilizadas 
         """
         self.chip_name = "/dev/gpiochip0"
         self.chip = gpiod.Chip(self.chip_name)
@@ -41,18 +40,18 @@ class Botao:
         estado = self.botoes[porta].get_value(porta)
         return estado == self.APERTADO  # True se pressionado, False se liberado
 
-# ===== TESTE =====
-if __name__ == "__main__":
-    botoes = Botao(portas=(Porta.P2, Porta.P4))  # Inicializa dois botões (P2 e P4)
+# # ===== TESTE =====
+# if __name__ == "__main__":
+#     botoes = Botao(portas=(Porta.P2, Porta.P4))  # Inicializa dois botões (P2 e P4)
 
-    print("Pressione Ctrl+C para sair.")
-    try:
-        while True:
-            estado_p2 = "PRESSIONADO" if botoes.ler_estado(Porta.P2) else "LIBERADO"
-            estado_p4 = "PRESSIONADO" if botoes.ler_estado(Porta.P4) else "LIBERADO"
+#     print("Pressione Ctrl+C para sair.")
+#     try:
+#         while True:
+#             estado_p2 = "PRESSIONADO" if botoes.ler_estado(Porta.P2) else "LIBERADO"
+#             estado_p4 = "PRESSIONADO" if botoes.ler_estado(Porta.P4) else "LIBERADO"
 
-            print(f"[P2] {estado_p2} | [P4] {estado_p4}")
-            time.sleep(0.1)
+#             print(f"[P2] {estado_p2} | [P4] {estado_p4}")
+#             time.sleep(0.1)
 
-    except KeyboardInterrupt:
-        print("\nEncerrando leitura dos botões.")
+#     except KeyboardInterrupt:
+#         print("\nEncerrando leitura dos botões.")
