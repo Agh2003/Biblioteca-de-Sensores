ae3824a763115cd36d364cf8894c68076c40bb4d
Andre Holtz <andregholtz81@gmail.com>
2025-10-22 18:41:44 -0300

    teste garra

--- exemplo/garraSensorCor.py
+++ exemplo/garraSensorCor.py
@@ -1,7 +1,13 @@
-from sensores import PCA9685, MG90S
+from sensores import PCA9685, MG90S, TCS34725
 import time
 
-def pegarObjeto():
+def pegarObjeto(initial_angle):
+    pca = PCA9685(mux_channel=5)
+
+    servo1 = MG90S(pca, channel=0)    
+
+    servo1.set_angle(initial_angle) 
+
     #tudo reto
     servo2 = MG90S(pca, channel=4)
     servo2.set_angle(50)
@@ -24,7 +30,8 @@ def pegarObjeto():
     servo2.set_angle(50)
 
     #direcao deposito
-    MG90S(pca, channel=0).set_angle(0)
+    for angle in range(initial_angle, 0, -7):
+        servo1.set_angle(angle)
 
     #abrir
     servo4.set_angle(0)
@@ -34,15 +41,21 @@ if __name__ == "__main__":
     # Inicializa PCA9685 no canal 0 do multiplexador
     pca = PCA9685(mux_channel=5)
 
-    servo1 = MG90S(pca, channel=0)    
+    while(True):
+        print('Coloque o objeto para leitura e pressione enter.')        
+        input()
 
-    servo1.set_angle(180) 
-    pegarObjeto()
+        cor = TCS34725(canal_mux=1).nome_cor()
+        print(cor)
+        
+        if cor == 'Vermelho':
+            pegarObjeto(180)
+        elif cor == 'Verde':
+            pegarObjeto(130)
+        elif cor == 'Azul':
+            pegarObjeto(80)
+        else:
+            print('Cor invalida')
+            input()
 
-    servo1.set_angle(130) 
-    pegarObjeto()
-
-    servo1.set_angle(80) 
-    pegarObjeto()
-
-    servo1.set_angle(130) 
+        MG90S(pca, 0).set_angle(130) 
